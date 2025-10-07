7da46e23b9609ab28c4837aa03d201d790eab03f
ataur <atauraahmad@gmail.com>
2025-10-07 22:23:38 +0100

    add array content to display

---
+++ app.js
@@ -0,0 +1,23 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  if (!new.target) {
+    throw new Error(
+      "You have to use the keyword 'new' to call up the constructor."
+    );
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+  };
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+console.log(myLibrary);
--- script.js
+++ script.js
@@ -1,13 +1,38 @@
+const container = document.querySelector(".container");
+const addButton = document.querySelector("button");
+const read = document.querySelector("#read");
+const form = document.querySelector(".form");
+
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   if (!new.target) {
-    Error("You have to use the keyword 'new' to call up the constructor.");
+    throw new Error(
+      "You have to use the keyword 'new' to call up the constructor."
+    );
   }
-
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
   this.info = function () {
-    return `${title} by ${author}, ${pages} pages, ${read} yet`;
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
   };
 }
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295 pages", "not read yet");
+addBookToLibrary("Enjoy your Life", "Al-areefy", "495 pages", "read");
+addBookToLibrary("To kill a mockingbird", "Harper Lee", "120 pages", "read");
+
+myLibrary.forEach((book) => {
+  container.innerHTML += `<p class="book">${book.title} by ${book.author}, ${book.pages}, ${book.read} <span class="remove" >*</span>  </p>`;
+});
+
+// addButton.addEventListener("click", () => {
+//   form.style.display = "block";
+// });
